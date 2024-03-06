6405ef821ca471a49d5da155bfd8f27e9c8341c5
Triet-li <kaneminhtriet101@gmail.com>
2024-03-06 09:26:35 +0700

    ok

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+class HomepageTest(SimpleTestCase):
+    def test_url_exit_at_correct_location(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+
+class AboutpageTest(SimpleTestCase):
+    def test_url_exit_at_correct_location(self):
+        response = self.client.get("/about/")
+        self.assertEqual(response.status_code, 200)
